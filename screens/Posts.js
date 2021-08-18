0a8e4111d1c545d57442b51e5a5164f95f05b5d9
Miguel Angel Romero <miguelromero717@gmail.com>
2021-08-17 21:20:42 -0500

    Implement list of posts by user

--- screens/Posts.js
+++ screens/Posts.js
@@ -1,19 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import ListItem from '../components/ListItem'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  list: {
+    alignSelf: "stretch",
   },
 });
 
-export default () => {
+export default ({ navigation }) => {
+  const userId = navigation.getParam('user_id')
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+
+    setPosts(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          style={styles.list}
+          data={posts.filter(p => p.userId === userId)}
+          keyExtractor={(p) => p.id}
+          renderItem={({ item }) => (
+            <ListItem
+              onPress={() => {
+                navigation.navigate("Posts", { user_id: item.id });
+              }}
+              title={item.title}
+            />
+          )}
+        />
+      )}
     </View>
   );
 };
