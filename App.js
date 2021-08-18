4b83ee09eb60b704fb332c04c8b4729d769b6945
Miguel Angel Romero <miguelromero717@gmail.com>
2021-08-17 20:16:41 -0500

    Set up screens and routes
    - Add Users, Posts, Detail screens
    - Refactor App.js to configure routes and screens

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import UsersScreen from './screens/Users'
+import PostsScreen from './screens/Posts'
+import DetailScreen from './screens/Detail'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Users: {
+    screen: UsersScreen,
   },
-});
+  Posts: {
+    screen: PostsScreen,
+  },
+  Detail: {
+    screen: DetailScreen
+  }
+}, {
+  initialRouteName: 'Users'
+})
+
+export default createAppContainer(AppNavigator)
